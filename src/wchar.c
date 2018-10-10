691168e37fd81e39e6d2d59aaab46c4c04ca3d9e
DavidMendelovits <David.Mendelovits@gmail.com>
2018-10-09 23:10:17 -0700

    hope to finish this up tomorrow

--- src/wchar.c
+++ src/wchar.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   string.c                                           :+:      :+:    :+:   */
+/*   wchar.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmendelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/09 18:50:09 by dmendelo          #+#    #+#             */
-/*   Updated: 2018/10/09 23:03:15 by dmendelo         ###   ########.fr       */
+/*   Created: 2018/10/09 22:45:04 by dmendelo          #+#    #+#             */
+/*   Updated: 2018/10/09 23:09:59 by dmendelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#define ASCII_MAX 0x7F
+#define _6_BIT_MAX 0x3F
+#define _16BIT_MASK 0xC080
+#define _24BIT_MASK 0xE08080
+#define _32BIT_MASK 0xF0808080
 
-void			string(t_opt *o, t_content *o)
-{
-	if (o->specifier == 's')
-	{
-		print_string(list, content);
-	}
-	else if (o->specifier == 'S')
-	{
-		print_wide_string(list, content);
-	}
-}
+void		print_wchar(wint_t);
+
+void		print_wstr(wchar_t);
